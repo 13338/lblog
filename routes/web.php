1e12f2c28390375c3159bcdb0085d71fae7302b4
Rostik G <g13338@gmail.com>
2018-05-19 17:44:34 +0300

    Routing

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostController@index')->name('index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('post', 'PostController');
+Route::resource('category', 'CategoryController');
