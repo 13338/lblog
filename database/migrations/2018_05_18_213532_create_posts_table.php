be096cab4ba4b270d4a6124fc0c36011228042a9
Rostik G <g13338@gmail.com>
2018-05-19 17:57:20 +0300

    Post Schema

--- database/migrations/2018_05_18_213532_create_posts_table.php
+++ database/migrations/2018_05_18_213532_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('category_id');
+            $table->string('name');
+            $table->text('content');
             $table->timestamps();
         });
     }
