5df97bbecac517c075dbca5a88cd66dc398451dc
Rostik G <g13338@gmail.com>
2018-05-20 02:45:00 +0300

    Show category

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $guarded = [];
+
+    /**
+     * Get the posts for the category.
+     */
+    public function posts()
+    {
+        return $this->hasMany('App\Post')->paginate(10);
+    }
 }
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use App\Category;
 use Illuminate\Http\Request;
 
@@ -50,7 +51,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return view('category.show', compact(['category']));
     }
 
     /**
