3f5ce97c8c15f2a40fedbd936b86370bec0d3578
Rostik G <g13338@gmail.com>
2018-05-19 20:55:50 +0300

    Post list

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::paginate(10);
+        return view ('post.index', compact(['posts']));
     }
 
     /**
---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    @foreach ($posts as $post)
+    <div class="col-md-8 mb-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><a href="{{ route('post.show', ['post' => $post->id]) }}">{{ $post->name }}</a></h5>
+        {{ $post->content }}
+      </div>
+      <div class="card-footer text-muted">
+        <span class="float-right"{!! (!empty($post->updated_at)) ? ' title="Last update '.$post->updated_at.'"' : '' !!}>
+          {{ $post->created_at }}
+        </span>
+      </div>
+    </div>
+    </div>
+    @endforeach
+
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      {{ $posts->links() }}
+    </div>
+  </div>
+</div>
+@endsection
