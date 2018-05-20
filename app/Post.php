6eb7c92153efb95fad60cf22412f764de1df692e
Rostik G <g13338@gmail.com>
2018-05-20 14:12:07 +0300

    Post::class methods

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $guarded = [];
+
+    /**
+     * Get the post's category.
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    function delete()
+    {
+        parent::delete();
+    }
 }
